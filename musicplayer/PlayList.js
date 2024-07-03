f87613b6bfbeae16aadcd09aa25443caf98e822c
Your Name <your_email@example.com>
2024-07-03 18:17:06 +0530

    Initial commit

---
+++ musicplayer/PlayList.js
@@ -0,0 +1,18 @@
+let songs = [
+    {
+        
+        songName:"Song 1",audio : "m1"
+    },
+    {
+        
+        songName:"Song 2",audio : "m2"
+    },
+    {
+        
+        songName:"Song 3",audio : "m3"
+    },
+    {
+        
+        songName:"Song 4",audio : "m4"
+    },
+]
